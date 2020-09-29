c00d83311705ca8b4308608fff9f73dee1985e61
Ahmed Gad <ahmed.f.gad@gmail.com>
2020-09-29 20:26:37 +0200

    PyGAD Lifecycle

---
+++ lifecycle.py
@@ -0,0 +1,48 @@
+import pygad
+import numpy
+
+function_inputs = [4,-2,3.5,5,-11,-4.7]
+desired_output = 44
+
+def fitness_func(solution, solution_idx):
+    output = numpy.sum(solution*function_inputs)
+    fitness = 1.0 / (numpy.abs(output - desired_output) + 0.000001)
+    return fitness
+
+fitness_function = fitness_func
+
+def on_start(ga_instance):
+    print("on_start()")
+
+def on_fitness(ga_instance, population_fitness):
+    print("on_fitness()")
+
+def on_parents(ga_instance, selected_parents):
+    print("on_parents()")
+
+def on_crossover(ga_instance, offspring_crossover):
+    print("on_crossover()")
+
+def on_mutation(ga_instance, offspring_mutation):
+    print("on_mutation()")
+
+def on_generation(ga_instance):
+    print("on_generation()")
+
+def on_stop(ga_instance, last_population_fitness):
+    print("on_stop")
+
+ga_instance = pygad.GA(num_generations=3,
+                       num_parents_mating=5,
+                       fitness_func=fitness_function,
+                       sol_per_pop=10,
+                       num_genes=len(function_inputs),
+                       on_start=on_start,
+                       on_fitness=on_fitness,
+                       on_parents=on_parents,
+                       on_crossover=on_crossover,
+                       on_mutation=on_mutation,
+                       on_generation=on_generation,
+                       on_stop=on_stop)
+
+ga_instance.run()
