05a069abf43146e7f8eb37f37c539523bf62ac9a
Ahmed Gad <ahmed.f.gad@gmail.com>
2021-01-15 11:18:02 -0500

    PyGAD 2.10.2
    A bug fix when save_best_solutions=True. Refer to this issue for more information: https://github.com/ahmedfgad/GeneticAlgorithmPython/issues/25

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Ahmed Fawzy Gad'
 author = 'Ahmed Fawzy Gad'
 
 # The full version, including alpha/beta/rc tags
-release = '2.10.1'
+release = '2.10.2'
 
 master_doc = 'index'
 
