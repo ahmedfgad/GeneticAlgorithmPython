719798f30147fc6674a5bd50fa4ade5c67d8adfe
Nath <nathanlloyd@protonmail.com>
2021-10-22 21:57:26 +0100

    Update ga.py
    Might seem a big change but the functions should all work the same.  I believe I improved naming convention.  And shortened lines to 80 columns to be more easily read on smaller screens.  Broke nested loop in mutation function to improve speed and instead used some mathematics involving floors and modulus.  Change operations like `a = a + 1` to `a += 1` as creates easier and briefer code.  Changed imports also to make code more readable. Changed all inline comments into somewhat useful docs strings so is helpful when someone calls `help()` on a function.

--- Tutorial Project/ga.py
+++ Tutorial Project/ga.py
@@ -1,45 +1,42 @@
-import numpy
+from numpy.random import uniform
+from numpy import inf, empty, sum, where, max, uint8
 
-def cal_pop_fitness(equation_inputs, pop):
-    # Calculating the fitness value of each solution in the current population.
-    # The fitness function calulates the sum of products between each input and its corresponding weight.
-    fitness = numpy.sum(pop*equation_inputs, axis=1)
-    return fitness
+def cal_pop_fitness(variables, population):
+    """ Calculating the fitness value of each solution in the current population.
+        The fitness function calulates the sum of products between each input
+        and its corresponding weight. """
+    return sum(population * variables, axis=1)
 
 def select_mating_pool(pop, fitness, num_parents):
-    # Selecting the best individuals in the current generation as parents for producing the offspring of the next generation.
-    parents = numpy.empty((num_parents, pop.shape[1]))
+    """ Selecting the best individuals in the current generation as parents for
+        producing the offspring of the next generation. """
+    parents = empty((num_parents, pop.shape[1]))
     for parent_num in range(num_parents):
-        max_fitness_idx = numpy.where(fitness == numpy.max(fitness))
-        max_fitness_idx = max_fitness_idx[0][0]
-        parents[parent_num, :] = pop[max_fitness_idx, :]
-        fitness[max_fitness_idx] = -99999999999
+        max_fitness_index = where(fitness == max(fitness))[0][0]
+        parents[parent_num, :] = pop[max_fitness_index, :]
+        fitness[max_fitness_index] = -inf
     return parents
 
 def crossover(parents, offspring_size):
-    offspring = numpy.empty(offspring_size)
-    # The point at which crossover takes place between two parents. Usually, it is at the center.
-    crossover_point = numpy.uint8(offspring_size[1]/2)
-
-    for k in range(offspring_size[0]):
-        # Index of the first parent to mate.
-        parent1_idx = k%parents.shape[0]
-        # Index of the second parent to mate.
-        parent2_idx = (k+1)%parents.shape[0]
-        # The new offspring will have its first half of its genes taken from the first parent.
-        offspring[k, 0:crossover_point] = parents[parent1_idx, 0:crossover_point]
-        # The new offspring will have its second half of its genes taken from the second parent.
-        offspring[k, crossover_point:] = parents[parent2_idx, crossover_point:]
+    """ The point at which crossover takes place between two parents.  Usually,
+        it is at the center.  Index of the first parent to mate.  Index of the
+        second parent to mate.  The new offspring will have its first half of
+        its genes taken from the first parent.  The new offspring will have its
+        second half of its genes taken from the second parent. """
+    offspring = empty(offspring_size)
+    co_point = uint8(offspring_size[1] / 2)
+    
+    for i in range(offspring_size[0]):
+        offspring[i, :co_point] = parents[i % parents.shape[0], :co_point]
+        offspring[i, co_point:] = parents[(i + 1) % parents.shape[0], co_point:]
     return offspring
 
-def mutation(offspring_crossover, num_mutations=1):
-    mutations_counter = numpy.uint8(offspring_crossover.shape[1] / num_mutations)
-    # Mutation changes a number of genes as defined by the num_mutations argument. The changes are random.
-    for idx in range(offspring_crossover.shape[0]):
-        gene_idx = mutations_counter - 1
-        for mutation_num in range(num_mutations):
-            # The random value to be added to the gene.
-            random_value = numpy.random.uniform(-1.0, 1.0, 1)
-            offspring_crossover[idx, gene_idx] = offspring_crossover[idx, gene_idx] + random_value
-            gene_idx = gene_idx + mutations_counter
+def mutation(offspring_crossover, mutations=1):
+    """ Mutation changes a number of genes as defined by the mutations argument.
+        The changes are random.  The random value to be added to the gene."""
+    mutations_counter = uint8(offspring_crossover.shape[1] / mutations)
+    for i in range(offspring_crossover.shape[0] * mutations):
+        gene_i = mutations_counter - 1 if i % mutations else gene_i
+        offspring_crossover[i // mutations, gene_i] += uniform(-1.0, 1.0, 1)
+        gene_i += mutations_counter
     return offspring_crossover
