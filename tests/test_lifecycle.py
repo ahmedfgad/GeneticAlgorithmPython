262463ee85259cc167cd3935ea331004bf3fcbe1
Ahmed Gad <ahmed.f.gad@gmail.com>
2023-04-27 19:57:27 -0400

    Test number of callbacks calls

---
+++ tests/test_lifecycle.py
@@ -0,0 +1,182 @@
+import pygad
+
+num_generations = 100
+
+def number_lifecycle_callback_functions_calls(stop_criteria=None,
+                                              on_generation_stop=None):
+    actual_num_callbacks_calls = 0
+
+    def fitness_func(ga_instanse, solution, solution_idx):
+        return 1
+
+    def on_start(ga_instance):
+        nonlocal actual_num_callbacks_calls
+        actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+    def on_fitness(ga_instance, population_fitness):
+        nonlocal actual_num_callbacks_calls
+        actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+    def on_parents(ga_instance, selected_parents):
+        nonlocal actual_num_callbacks_calls
+        actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+    def on_crossover(ga_instance, offspring_crossover):
+        nonlocal actual_num_callbacks_calls
+        actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+    def on_mutation(ga_instance, offspring_mutation):
+        nonlocal actual_num_callbacks_calls
+        actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+    def on_generation(ga_instance):
+        nonlocal actual_num_callbacks_calls
+        actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+        
+        if on_generation_stop:
+            if ga_instance.generations_completed == on_generation_stop:
+                return "stop"
+
+    def on_stop(ga_instance, last_population_fitness):
+        nonlocal actual_num_callbacks_calls
+        actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+    ga_instance = pygad.GA(num_generations=num_generations,
+                           num_parents_mating=5,
+                           fitness_func=fitness_func,
+                           sol_per_pop=10,
+                           num_genes=5,
+                           on_start=on_start,
+                           on_fitness=on_fitness,
+                           on_parents=on_parents,
+                           on_crossover=on_crossover,
+                           on_mutation=on_mutation,
+                           on_generation=on_generation,
+                           on_stop=on_stop,
+                           stop_criteria=stop_criteria,
+                           suppress_warnings=True)
+
+    ga_instance.run()
+
+    # The total number is:
+        # 1 [for on_start()] +
+        # num_generations [for on_fitness()] +
+        # num_generations [for on_parents()] +
+        # num_generations [for on_crossover()] +
+        # num_generations [for on_mutation()] +
+        # num_generations [for on_generation()] +
+        # 1 [for on_stop()]
+        # = 1 + num_generations * 5 + 1
+
+    # Use 'generations_completed' instead of 'num_generations' because the evolution may stops in the on_generation() callback.
+    expected_num_callbacks_calls = 1 + ga_instance.generations_completed * 5 + 1
+
+    print("Expected number of callbacks calls is {expected_num_callbacks_calls}.".format(expected_num_callbacks_calls=expected_num_callbacks_calls))
+    print("Actual number of callbacks calls is {actual_num_callbacks_calls}.".format(actual_num_callbacks_calls=actual_num_callbacks_calls))
+    return actual_num_callbacks_calls, expected_num_callbacks_calls
+
+def number_lifecycle_callback_methods_calls(stop_criteria=None,
+                                            on_generation_stop=None):
+    actual_num_callbacks_calls = 0
+
+    class Callbacks:
+        def fitness_func(self, ga_instanse, solution, solution_idx):
+            return 1
+
+        def on_start(self, ga_instance):
+            nonlocal actual_num_callbacks_calls
+            actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+        def on_fitness(self, ga_instance, population_fitness):
+            nonlocal actual_num_callbacks_calls
+            actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+        def on_parents(self, ga_instance, selected_parents):
+            nonlocal actual_num_callbacks_calls
+            actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+        def on_crossover(self, ga_instance, offspring_crossover):
+            nonlocal actual_num_callbacks_calls
+            actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+        def on_mutation(self, ga_instance, offspring_mutation):
+            nonlocal actual_num_callbacks_calls
+            actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+        def on_generation(self, ga_instance):
+            nonlocal actual_num_callbacks_calls
+            actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+            if on_generation_stop:
+                if ga_instance.generations_completed == on_generation_stop:
+                    return "stop"
+
+        def on_stop(self, ga_instance, last_population_fitness):
+            nonlocal actual_num_callbacks_calls
+            actual_num_callbacks_calls = actual_num_callbacks_calls + 1
+
+    Callbacks_obj = Callbacks()
+    ga_instance = pygad.GA(num_generations=num_generations,
+                           num_parents_mating=5,
+                           fitness_func=Callbacks_obj.fitness_func,
+                           sol_per_pop=10,
+                           num_genes=5,
+                           on_start=Callbacks_obj.on_start,
+                           on_fitness=Callbacks_obj.on_fitness,
+                           on_parents=Callbacks_obj.on_parents,
+                           on_crossover=Callbacks_obj.on_crossover,
+                           on_mutation=Callbacks_obj.on_mutation,
+                           on_generation=Callbacks_obj.on_generation,
+                           on_stop=Callbacks_obj.on_stop,
+                           stop_criteria=stop_criteria,
+                           suppress_warnings=True)
+
+    ga_instance.run()
+
+    # The total number is:
+        # 1 [for on_start()] +
+        # num_generations [for on_fitness()] +
+        # num_generations [for on_parents()] +
+        # num_generations [for on_crossover()] +
+        # num_generations [for on_mutation()] +
+        # num_generations [for on_generation()] +
+        # 1 [for on_stop()]
+        # = 1 + num_generations * 5 + 1
+
+    # Use 'generations_completed' instead of 'num_generations' because the evolution may stops in the on_generation() callback.
+    expected_num_callbacks_calls = 1 + ga_instance.generations_completed * 5 + 1
+
+    print("Expected number of callbacks calls is {expected_num_callbacks_calls}.".format(expected_num_callbacks_calls=expected_num_callbacks_calls))
+    print("Actual number of callbacks calls is {actual_num_callbacks_calls}.".format(actual_num_callbacks_calls=actual_num_callbacks_calls))
+    return actual_num_callbacks_calls, expected_num_callbacks_calls
+
+def test_number_lifecycle_callback_functions_calls():
+    actual, expected = number_lifecycle_callback_functions_calls()
+    
+    assert actual == expected
+
+def test_number_lifecycle_callback_functions_calls_stop_criteria():
+    actual, expected = number_lifecycle_callback_functions_calls(on_generation_stop=30)
+
+    assert actual == expected
+
+def test_number_lifecycle_callback_methods_calls():
+    actual, expected = number_lifecycle_callback_methods_calls()
+    
+    assert actual == expected
+
+def test_number_lifecycle_callback_methods_calls_stop_criteria():
+    actual, expected = number_lifecycle_callback_methods_calls(on_generation_stop=30)
+
+    assert actual == expected
+
+if __name__ == "__main__":
+    print()
+    test_number_lifecycle_callback_functions_calls()
+    print()
+    test_number_lifecycle_callback_functions_calls_stop_criteria()
+    print()
+    test_number_lifecycle_callback_methods_calls()
+    print()
+    test_number_lifecycle_callback_methods_calls_stop_criteria()
+    print()
--- tests/test_number_fitness_function_calls.py
+++ tests/test_number_fitness_function_calls.py
@@ -9,8 +9,10 @@ num_generations = 100
 sol_per_pop = 10
 num_parents_mating = 5
 
-def number_calls_fitness_function(keep_elitism, 
-                                  keep_parents):
+def number_calls_fitness_function(keep_elitism=1, 
+                                  keep_parents=-1,
+                                  mutation_type="random",
+                                  mutation_percent_genes="default"):
 
     actual_num_fitness_calls = 0
     def fitness_func(ga, solution, idx):
@@ -23,6 +25,8 @@ def number_calls_fitness_function(keep_elitism,
                             num_genes=6,
                             num_parents_mating=num_parents_mating,
                             fitness_func=fitness_func,
+                            mutation_type=mutation_type,
+                            mutation_percent_genes=mutation_percent_genes,
                             keep_elitism=keep_elitism,
                             keep_parents=keep_parents,
                             suppress_warnings=True)
@@ -32,43 +36,69 @@ def number_calls_fitness_function(keep_elitism,
         if keep_parents == 0:
             # 10 (for initial population) + 100*10 (for other generations) = 1010
             expected_num_fitness_calls = sol_per_pop + num_generations * sol_per_pop
+            if mutation_type == "adaptive":
+                expected_num_fitness_calls += num_generations * sol_per_pop
         elif keep_parents == -1:
             # 10 (for initial population) + 100*num_parents_mating (for other generations)
             expected_num_fitness_calls = sol_per_pop + num_generations * (sol_per_pop - num_parents_mating)
+            if mutation_type == "adaptive":
+                expected_num_fitness_calls += num_generations * (sol_per_pop - num_parents_mating)
         else:
             # 10 (for initial population) + 100*keep_parents (for other generations)
             expected_num_fitness_calls = sol_per_pop + num_generations * (sol_per_pop - keep_parents)
+            if mutation_type == "adaptive":
+                expected_num_fitness_calls += num_generations * (sol_per_pop - keep_parents)
     else:
         # 10 (for initial population) + 100*keep_elitism (for other generations)
         expected_num_fitness_calls = sol_per_pop + num_generations * (sol_per_pop - keep_elitism)
+        if mutation_type == "adaptive":
+            expected_num_fitness_calls += num_generations * (sol_per_pop - keep_elitism)
 
     print("Expected number of fitness function calls is {expected_num_fitness_calls}.".format(expected_num_fitness_calls=expected_num_fitness_calls))
     print("Actual number of fitness function calls is {actual_num_fitness_calls}.".format(actual_num_fitness_calls=actual_num_fitness_calls))
     return actual_num_fitness_calls, expected_num_fitness_calls
 
 def test_number_calls_fitness_function_default_keep():
-    actual, expected = number_calls_fitness_function(keep_elitism=1, 
-                                                                                          keep_parents=-1)
+    actual, expected = number_calls_fitness_function()
     assert actual == expected
 
 def test_number_calls_fitness_function_no_keep():
     actual, expected = number_calls_fitness_function(keep_elitism=0, 
-                                                                                          keep_parents=0)
+                                                     keep_parents=0)
     assert actual == expected
 
 def test_number_calls_fitness_function_keep_elitism():
     actual, expected = number_calls_fitness_function(keep_elitism=3, 
-                                                                                          keep_parents=0)
+                                                     keep_parents=0)
     assert actual == expected
 
 def test_number_calls_fitness_function_keep_parents():
     actual, expected = number_calls_fitness_function(keep_elitism=0, 
-                                                                                         keep_parents=4)
+                                                     keep_parents=4)
     assert actual == expected
 
 def test_number_calls_fitness_function_both_keep():
     actual, expected = number_calls_fitness_function(keep_elitism=3, 
-                                                                                         keep_parents=4)
+                                                     keep_parents=4)
+    assert actual == expected
+
+def test_number_calls_fitness_function_no_keep_adaptive_mutation():
+    actual, expected = number_calls_fitness_function(keep_elitism=0, 
+                                                     keep_parents=0,
+                                                     mutation_type="adaptive",
+                                                     mutation_percent_genes=[10, 5])
+    assert actual == expected
+
+def test_number_calls_fitness_function_default_adaptive_mutation():
+    actual, expected = number_calls_fitness_function(mutation_type="adaptive",
+                                                     mutation_percent_genes=[10, 5])
+    assert actual == expected
+
+def test_number_calls_fitness_function_both_keep_adaptive_mutation():
+    actual, expected = number_calls_fitness_function(keep_elitism=3, 
+                                                     keep_parents=4,
+                                                     mutation_type="adaptive",
+                                                     mutation_percent_genes=[10, 5])
     assert actual == expected
 
 if __name__ == "__main__":
@@ -82,4 +112,10 @@ if __name__ == "__main__":
     test_number_calls_fitness_function_keep_parents()
     print()
     test_number_calls_fitness_function_both_keep()
+    print()
+    test_number_calls_fitness_function_no_keep_adaptive_mutation()
+    print()
+    test_number_calls_fitness_function_default_adaptive_mutation()
+    print()
+    test_number_calls_fitness_function_both_keep_adaptive_mutation()
     print()
