376123671e380ed6af13697d34173fe1b954785a
Ahmed Gad <ahmed.f.gad@gmail.com>
2023-06-20 13:17:06 -0400

    Use hatchling instead of setuptools

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-# import setuptools  
+import setuptools  
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-# setuptools.setup(  
-#     name="pygad",  
-#     version="3.1.0",  
-#     author="Ahmed Fawzy Gad",
-#     install_requires=["numpy", "matplotlib", "cloudpickle",],
-#     author_email="ahmed.f.gad@gmail.com",  
-#     description="PyGAD: A Python Library for Building the Genetic Algorithm and Training Machine Learning Algoithms (Keras & PyTorch).",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/ahmedfgad/GeneticAlgorithmPython",
-#     packages=setuptools.find_packages())
+setuptools.setup(  
+    name="pygad",  
+    version="3.1.0",  
+    author="Ahmed Fawzy Gad",
+    install_requires=["numpy", "matplotlib", "cloudpickle",],
+    author_email="ahmed.f.gad@gmail.com",  
+    description="PyGAD: A Python Library for Building the Genetic Algorithm and Training Machine Learning Algoithms (Keras & PyTorch).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ahmedfgad/GeneticAlgorithmPython",
+    packages=setuptools.find_packages())
