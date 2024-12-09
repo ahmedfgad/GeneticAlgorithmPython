516ac20ba99504432f206086cc9651d1b0174c5e
Ahmed Gad <ahmed.f.gad@gmail.com>
2024-12-09 12:06:33 -0500

    Read the requirements from the .txt file

--- setup.py
+++ setup.py
@@ -3,11 +3,15 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+# Read the requirements from the requirements.txt file
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(  
     name="pygad",  
     version="3.3.1",  
     author="Ahmed Fawzy Gad",
-    install_requires=["numpy", "matplotlib", "cloudpickle",],
+    install_requires=requirements,
     author_email="ahmed.f.gad@gmail.com",  
     description="PyGAD: A Python Library for Building the Genetic Algorithm and Training Machine Learning Algoithms (Keras & PyTorch).",
     long_description=long_description,
